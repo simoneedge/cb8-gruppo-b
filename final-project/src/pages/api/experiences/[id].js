189eb419a2738b3907ecb15721e8c9080fd265f9
paolospoto <paolo.spoto@gmail.com>
2024-04-03 00:54:26 +0200

    feat: connect endpoints to database

--- final-project/src/pages/api/experiences/[id].js
+++ final-project/src/pages/api/experiences/[id].js
@@ -1,10 +1,28 @@
-export default async function handler(req, res) {
-  const {
-    method,
-    query: { id },
-  } = req;
+import { ObjectId } from "mongodb";
+import clientPromise from "../../../../utils/mongodb";
 
-  if (method === "GET") {
-    res.status(200).json({ message: "SINGOLA ESPERIENZA CON ID: " + id });
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    try {
+      const client = await clientPromise;
+      const db = client.db("final-project");
+
+      if (!req.query.id) {
+        return res.status(400).json({ error: "ID is required" });
+      }
+
+      // Converti l'ID da stringa ad ObjectId per la query
+      const id = new ObjectId(req.query.id);
+      const data = await db.collection("experiences").findOne({ _id: id });
+
+      if (data) {
+        res.status(200).json(data);
+      } else {
+        res.status(404).json({ error: "Not Found" });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
   }
 }
