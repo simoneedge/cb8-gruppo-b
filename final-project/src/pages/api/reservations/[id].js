7e4f9181ecffcca09cf40281de6f15cd4b99682a
paolospoto <paolo.spoto@gmail.com>
2024-04-03 23:36:22 +0200

    feat: add post endpoint for reservations

--- final-project/src/pages/api/reservations/[id].js
+++ final-project/src/pages/api/reservations/[id].js
@@ -2,11 +2,10 @@ import { ObjectId } from "mongodb";
 import clientPromise from "../../../../utils/mongodb";
 
 export default async function handler(req, res) {
+  const client = await clientPromise;
+  const db = client.db("final-project");
   if (req.method === "GET") {
     try {
-      const client = await clientPromise;
-      const db = client.db("final-project");
-
       if (!req.query.id) {
         return res.status(400).json({ error: "ID is required" });
       }
@@ -39,6 +38,20 @@ export default async function handler(req, res) {
         res.status(200).json(reservations);
       } else {
         res.status(404).json({ error: "Not Found" });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  } else if (req.method === "POST") {
+    try {
+      const reservation = req.body;
+      reservation.userId = req.query.id;
+      const result = await db.collection("reservations").insertOne(reservation);
+      if (result.acknowledged) {
+        res.status(201).json({ message: "Reservation created" });
+      } else {
+        res.status(400).json({ error: "Reservation not created" });
       }
     } catch (error) {
       console.error(error);
