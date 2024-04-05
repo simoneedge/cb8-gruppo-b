6deed446143c1bc3af5e6a239f8c478d01ae185d
FedericaI7 <federicaiuvara3@gmail.com>
2024-04-05 13:10:08 +0200

    feat: create Experience Page and dynamic page, and create module.scss

---
+++ final-project/src/pages/experience/[name].jsx
@@ -0,0 +1,9 @@
+import styles from "../../styles/ExperienceDetail.module.scss";
+
+export default function ExperienceDetail() {
+  return (
+    <div className={styles.ExperienceDetail}>
+      <h1>Experience Detail</h1>
+    </div>
+  );
+}
