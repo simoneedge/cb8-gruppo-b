b6ae9a50d35f14bd95859880885517de648f67eb
AlessioR7 <restivoalessio22@gmail.com>
2024-04-05 18:09:07 +0200

    feat: add slider component

---
+++ final-project/src/components/slider/ImageSlider.jsx
@@ -0,0 +1,43 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import styles from "./index.module.scss";
+
+const ImageSlider = ({ images }) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3500,
+  };
+
+  return (
+    <div>
+      <Slider {...settings}>
+        {images.map((image, index) => (
+          <div key={index} className={styles.Slider}>
+            <img
+              src={image}
+              alt="image"
+              style={{
+                height: "400px",
+                width: "100%",
+                objectFit: "cover",
+                borderRadius: "30px",
+                padding: "2px",
+              }}
+            />
+          </div>
+        ))}
+      </Slider>
+      <div className={styles.dot}>
+        <span>l</span>
+      </div>
+    </div>
+  );
+};
+
+export default ImageSlider;
