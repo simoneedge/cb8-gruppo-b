3d090f9f54ecb3a6173b7fb2d62c2851cdd6ba37
Eli07Romi <elia.dilorenzo@yahoo.it>
2024-04-08 16:30:06 +0200

    aboutUsPage

---
+++ final-project/src/pages/aboutUs/index.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to Sicily Experience App!</p>
+      <p>
+        At Sicily Experience, we are passionate about showcasing the beauty,
+        culture, and traditions of Sicily. Our goal is to provide users with
+        unforgettable experiences that allow them to immerse themselves in the
+        rich history and vibrant atmosphere of this magnificent island.
+      </p>
+      <h2>Our Team</h2>
+      <p>Meet the dedicated rockstart who work on the Sicily Experience App:</p>
+      <ul>
+        <li>
+          <a href="https://www.linkedin.com/in/federica-iuvara">
+            Federica Iuvara
+          </a>
+          - UX/UI Designer
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/paolo-spoto-894110227/">
+            Paolo Spoto
+          </a>
+          - BE Developer
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/rita-teresa-landino-a7a54b2b4/">
+            Rita Iandolino
+          </a>
+          - BE Developer
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/alessio-restivo/">
+            Alessio Restivo
+          </a>
+          - FE Developer
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/elia-di-lorenzo-526040235/">
+            Elia Di Lorenzo
+          </a>
+          - FE Developer
+        </li>
+      </ul>
+      <h2>Our Values</h2>
+      <ul>
+        <li>
+          Authenticity: We strive to offer authentic experiences that truly
+          capture the essence of Sicily.
+        </li>
+        <li>
+          Community: We believe in fostering a sense of community among our
+          users, connecting them with locals and fellow travelers alike.
+        </li>
+        <li>
+          Quality: We are committed to delivering high-quality experiences that
+          exceed our users' expectations.
+        </li>
+        <li>
+          Inclusivity: We welcome everyone to explore Sicily with us, regardless
+          of background or experience. Our hosts are fluent in English and
+          highly trained to offer services to both locals and tourists.
+        </li>
+      </ul>
+      <h2>Contact Information</h2>
+      <p>
+        If you have any questions or inquiries, please don't hesitate to contact
+        us:
+      </p>
+      <p>Address: Via Roma, 123 - Palermo, Sicily</p>
+      <p>Phone: +39 123 456 789</p>
+    </div>
+  );
+}
