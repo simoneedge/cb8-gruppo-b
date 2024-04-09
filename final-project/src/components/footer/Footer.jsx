140b54fae257cee49561574187d46f8fd96c1d43
FedericaI7 <federicaiuvara3@gmail.com>
2024-04-09 12:05:34 +0200

    feat: create style and links in Footer.jsx

--- final-project/src/components/footer/Footer.jsx
+++ final-project/src/components/footer/Footer.jsx
@@ -1,11 +1,61 @@
 import styles from "../../styles/Footer.module.scss";
-
+import Image from "next/image";
+import Link from "next/link";
 import { IconBrandGithub } from "@tabler/icons-react";
 
 const Footer = () => {
   return (
     <footer className={styles.Footer}>
-      <h1>ciao</h1>
+      <div className={styles.leftSection}>
+        <Image
+          src="/logosicily.svg"
+          width={100}
+          height={100}
+          className={styles.logoImage}
+          alt="logo"
+          priority
+        />
+
+        <div>
+          <h3>
+            "Sicily Experience" is a group final project developed during the
+            Edgemony Bootcamp in April 2024.
+          </h3>
+          <p>#Final Project</p>
+          <p>#Edgemony Bootcamp</p>
+        </div>
+      </div>
+
+      <div className={styles.rightSection}>
+        <h3>Contributors</h3>
+        <div className={styles.containerContributors}>
+          <div className={styles.contributor}>
+            <IconBrandGithub />
+            <p>Federica Iuvara</p>
+            <Link href="https://github.com/FedericaI7">Github</Link>
+          </div>
+          <div className={styles.contributor}>
+            <IconBrandGithub />
+            <p>Paolo Spoto</p>
+            <Link href="https://github.com/paolospoto">Github</Link>
+          </div>
+          <div className={styles.contributor}>
+            <IconBrandGithub />
+            <p>Alessio Restivo</p>
+            <Link href="https://github.com/AlessioR7">Github</Link>
+          </div>
+          <div className={styles.contributor}>
+            <IconBrandGithub />
+            <p>Rita Iandolino</p>
+            <Link href="https://github.com/RitaL1990">Github</Link>
+          </div>
+          <div className={styles.contributor}>
+            <IconBrandGithub />
+            <p>Elia Di Lorenzo</p>
+            <Link href="https://github.com/Eli07Romi">Github</Link>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
