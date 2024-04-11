68a74cb7bab1daec4dc54b0efe40bcfaf383612f
paolospoto <paolo.spoto@gmail.com>
2024-04-11 15:49:29 +0200

    feat: signIn button

---
+++ final-project/src/components/signInButton/SignInButton.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Button } from "@mantine/core";
+
+const SignInButton = () => {
+  const { data: session } = useSession();
+  return (
+    <Button
+      color="purple"
+      onClick={() => {
+        session ? signOut() : signIn();
+      }}
+    >
+      {session ? "SIGN OUT" : "SIGN IN"}
+    </Button>
+  );
+};
+
+export default SignInButton;
